ddb4fe4ab2b311d8fa369959ffe8cd8275b5fee6
eung7 <wannabeeung@gmail.com>
2022-07-06 00:16:51 +0900

    Implementing MapView

---
+++ NearMe/Views/MapView.swift
@@ -0,0 +1,23 @@
+//
+//  MapView.swift
+//  NearMe
+//
+//  Created by 김응철 on 2022/07/05.
+//
+
+import SwiftUI
+import MapKit
+import UIKit
+
+struct MapView: UIViewRepresentable {
+    func makeUIView(context: Context) -> MKMapView {
+        let map = MKMapView()
+        map.showsUserLocation = true
+//        map.delegate = context.coordinator
+        return map
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        
+    }
+}
