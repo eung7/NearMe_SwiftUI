a432688213e7302c10446cbdc9d6ca0768c2237b
eung7 <wannabeeung@gmail.com>
2022-07-05 23:25:03 +0900

    Initial Commit

---
+++ NearMe/NearMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearMeApp.swift
+//  NearMe
+//
+//  Created by 김응철 on 2022/07/05.
+//
+
+import SwiftUI
+
+@main
+struct NearMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
