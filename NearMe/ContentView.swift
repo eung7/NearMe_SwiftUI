828d0e4b590c8b0dc93a5947ab0ec2f64359df2e
eung7 <wannabeeung@gmail.com>
2022-07-06 19:11:02 +0900

    Retrieving Nearby Landmarks

--- NearMe/ContentView.swift
+++ NearMe/ContentView.swift
@@ -6,12 +6,43 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
     @ObservedObject private var locationManager = LocationManager()
+    @State private var search: String = ""
+    
+    private func getNearByLandmarks() {
+        guard let location = locationManager.location else { return }
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = search
+        request.region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
+        
+        let search = MKLocalSearch(request: request)
+        search.start { response, error in
+            guard let response = response, error == nil else { return }
+            let mapItems = response.mapItems
+            mapItems.map {
+                print($0.placemark)
+            }
+        }
+    }
     
     var body: some View {
-        MapView()
+        ZStack(alignment: .top) {
+            MapView()
+            
+            TextField("Search",
+                      text: $search,
+                      onEditingChanged: { _ in
+                
+            },
+                      onCommit: {
+                getNearByLandmarks()
+            })
+                .textFieldStyle(.roundedBorder)
+                .padding()
+        }
     }
 }
 
